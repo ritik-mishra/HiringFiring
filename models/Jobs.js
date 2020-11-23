34fad995b8121252f58c15ad489ed4a31509165c
aditi <aditishreya2929@gmail.com>
2020-11-23 17:51:23 +0530

    Updated schema for add job

--- models/Jobs.js
+++ models/Jobs.js
@@ -4,10 +4,10 @@ const { Schema } = mongoose;
 const jobSchema = new Schema({
     jobId: String,
     companyName: String,
-    // jobTitle: String,
-    // JobLink: String,
-    // batch: String,
-    // PostedOn: { type: Date, default: Date.now },
+    jobTitle: String,
+    JobLink: String,
+    batch: String,
+    PostedOn: { type: Date, default: Date.now },
     PostedBy: String
 });
 
