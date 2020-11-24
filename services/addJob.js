71ee0f3de258b0bc9f03c3d009fd4d02b325a59c
namrata111f <48027172+namrata111f@users.noreply.github.com>
2020-11-24 13:33:53 +0530

    Delete addJob.js
    Unnecessary file not needed anymore.

--- services/addJob.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const Job = mongoose.model('jobs');
-const { v4: uuidv4 } = require('uuid');
-
-// const newId = uuidv4();
-// new Job({ jobId: newId, companyName: 'Google', PostedBy: 'Shreya'}).save();
