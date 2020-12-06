383adf9294fd78811a59cab4db5b12776ae33a55
Nimmi <namrata111f@gmail.com>
2020-12-06 15:32:59 +0530

    removed unecessary console logs and client config folder

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
-import url from '../configClient/url';
 
 import Header from './Header';
 import Landing from './Landing';
