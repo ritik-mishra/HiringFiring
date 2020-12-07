3436f95ff0bb126f53b93a2fad1c22e9e95187d7
Nimmi <namrata111f@gmail.com>
2020-12-07 13:20:37 +0530

    testing with env variables

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 module.exports = {
-    googleClientID: '107101271539-pv6tssefdueq32t8aureb32t5v3c1oc8.apps.googleusercontent.com',
-    googleClientSecret: '7TYUGIZP9QrO9fqf7wIVgjA-',
-    mongoURI: 'mongodb+srv://Nimmi:zKhSVmsOYVTDT83X@hiringfiring.setfg.mongodb.net/test?retryWrites=true&w=majority',
-    cookieKey: 'iahjkgthkloiuyhbvfredswdcftgdesrvbh',
-    baseURL: '/hiring-firing'
+    googleClientID: process.env.googleClientID,
+    googleClientSecret: process.env.googleClientSecret,
+    mongoURI: process.env.mongoURI,
+    cookieKey: process.env.cookieKey,
+    baseURL: process.env.baseURL
 };
