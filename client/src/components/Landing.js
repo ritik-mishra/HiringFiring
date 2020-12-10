d1de5d065b11d778d6e398eb2425929ae62120d9
namrata111f <48027172+namrata111f@users.noreply.github.com>
2020-12-10 12:09:44 +0530

    Revert "Sonu frontend update"

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,14 +3,11 @@ import './Landing.css'
 
 const Landing = () => {
     return (
-        <div>
-            <div style={{ textAlign: 'center' }}>
-                <div style={{ fontSize: "30px", marginTop: "50px", color: "#808080", fontWeight: "bold" }} >Welcome to the Hiring Firing!!</div>
-            </div>
-            <div >
-                <div style={{ display: 'flex', justifyContent: 'center', fontSize: "50px", marginTop: "50px" }} className="GOTtext"> Winter is coming</div>
-                <div style={{ display: 'flex', justifyContent: 'center', fontSize: "40px", marginTop: "20px" }} className="GOTtext"> So are the jobs</div>
-            </div>
+        <div style={{ textAlign: 'center' }}>
+            <h1>
+                Hiring Firing!!
+            </h1>
+            Welcome to our website!
         </div>
     );
 };
