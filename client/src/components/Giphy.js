8b1ffa6d7972a1fe8c21ffecf85f72e85e7be343
sonu verma <vsonu024@gmail.com>
2021-01-08 21:33:01 +0530

    giphy

--- client/src/components/Giphy.js
+++ client/src/components/Giphy.js
@@ -7,16 +7,16 @@ function rand() {
     return Math.round(Math.random() * 20) - 10;
 }
 
-function getModalStyle() {
-    const top = 50 + rand();
-    const left = 50 + rand();
+// function getModalStyle() {
+//     const top = 50 + rand();
+//     const left = 50 + rand();
 
-    return {
-        top: `${top}%`,
-        left: `${left}%`,
-        transform: `translate(-${top}%, -${left}%)`,
-    };
-}
+//     return {
+//         top: `${top}%`,
+//         left: `${left}%`,
+//         transform: `translate(-${top}%, -${left}%)`,
+//     };
+// }
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -35,12 +35,10 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function NotificationModal(props) {
-    console.log(props);
 
     const classes = useStyles();
-    const hover = `Set reminder`;
     // getModalStyle is not a pure function, we roll the style only on the first render
-    const [modalStyle] = React.useState(getModalStyle);
+    // const [modalStyle] = React.useState(getModalStyle);
     const [open, setOpen] = React.useState(props.isOpen);
     useEffect(async () => {
         // Your code here
@@ -52,8 +50,8 @@ export default function NotificationModal(props) {
         setOpen(false);
     };
     const body = (
-        <div style={{ marginTop: "6rem", marginLeft: "24rem" }} >
-            <img style={{ height: "30rem", width: "40rem", padding: "2px" }} className={classes.paper} src={giphy1} alt="description of gif" />
+        <div style={{ marginTop: "10%", marginLeft: "33%" }} >
+            <img style={{ height: "30rem", width: "40rem", padding: "2px", opacity: "10%" }} className={classes.paper} src={giphy1} alt="description of gif" />
         </div>
     );
 
